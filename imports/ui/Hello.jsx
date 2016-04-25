5cd4cffd1a1c581e6bbc6064093461e2155ae6cd
Peter Wang <happypeter1983@gmail.com>
2016-04-25 11:35:45 +0800

    使用 Material-UI 组件 RaisedButton

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 class Hello extends Component {
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <RaisedButton label='material-ui' primary={true} />
+      </MuiThemeProvider>
     );
   }
 }
