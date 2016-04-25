460baee4a6af067462ffea84a66cec2577bed8e1
Peter Wang <happypeter1983@gmail.com>
2016-04-25 11:33:43 +0800

    使用 Material-UI 的依赖包

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import Hello from '../imports/ui/Hello.jsx';
 
 Meteor.startup(() => {
+  injectTapEventPlugin();
   render(<Hello />, document.getElementById('app-container'));
 });
