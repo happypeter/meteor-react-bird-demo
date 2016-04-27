f38201f06d395aac3138602782dc623f41d7a7cf
Peter Wang <happypeter1983@gmail.com>
2016-04-27 16:16:25 +0800

    编写嵌套路由

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import SignUp from '../../ui/SignUp.jsx';
 import LogIn from '../../ui/LogIn.jsx';
 import Home from '../../ui/Home.jsx';
+import App from '../../ui/App.jsx';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
-    <Route path='/' component={Home} />
-    <Route path="/signup" component={SignUp} />
-    <Route path="/login" component={LogIn} />
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={LogIn} />
+    </Route>
   </Router>
 );
