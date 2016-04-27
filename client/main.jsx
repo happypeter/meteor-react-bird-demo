2915d99bb424c0ec4def759e26546a80d843f574
Peter Wang <happypeter1983@gmail.com>
2016-04-27 15:41:31 +0800

    渲染路由组件

--- client/main.jsx
+++ client/main.jsx
@@ -4,9 +4,9 @@ import { Meteor } from 'meteor/meteor';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import Hello from '../imports/ui/Hello.jsx';
+import { renderRoutes } from '../imports/startup/client/routes.jsx';
 
 Meteor.startup(() => {
   injectTapEventPlugin();
-  render(<Hello />, document.getElementById('app-container'));
+  render(renderRoutes(), document.getElementById('app-container'));
 });
