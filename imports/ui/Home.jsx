5c9a0565220a9e7931f57502d107557b8ad8044a
Peter Wang <happypeter1983@gmail.com>
2016-05-03 17:20:57 +0800

    美化 Home 组件

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import Radium from 'radium';
 
 class Home extends Component {
+  getStyles() {
+    return {
+      root: {
+        flexGrow: '1',
+        backgroundImage: 'url(/images/home-bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      },
+      slogan: {
+        textAlign: 'center',
+        color: '#fff',
+        textTransform: 'uppercase',
+        fontSize: '25px',
+        fontWeight: '500',
+        width: '100%',
+        paddingTop: '50px',
+        paddingBottom: '50px',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        '@media (min-width: 600px)': {
+          fontSize: '50px',
+          paddingBottom: '100px',
+          paddingTop: '100px'
+        }
+      }
+    };
+  }
   render() {
+    const styles = this.getStyles();
     return (
-      <div>
-        This is home page.
+      <div style={styles.root}>
+        <div style={styles.slogan}>
+          chat room demo
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+export default Radium(Home);
+
