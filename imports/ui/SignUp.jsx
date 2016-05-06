433433cca4310606cb05cab90330f3e143ab42c5
Peter Wang <happypeter1983@gmail.com>
2016-05-06 10:37:18 +0800

    创建注册表单

--- imports/ui/SignUp.jsx
+++ imports/ui/SignUp.jsx
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Radium from 'radium';
+
+import typography from './styles/typography';
 
 class SignUp extends Component {
+  getStyles() {
+    return {
+      root: {
+        minHeight: '400px',
+        textAlign: 'center',
+        padding: '6em 2em',
+        '@media (min-width: 500px)': {
+          width: '500px',
+          margin: '0 auto'
+        }
+      },
+      form: {
+        margin: '30px auto 0'
+      },
+      textField: {
+        display: 'block',
+        width: '100%'
+      },
+      floatingLabel: {
+        fontSize: typography.fontSmallSize
+      },
+      label: {
+        lineHeight: '50px',
+        fontWeight: '600',
+        fontSize: typography.fontSmallSize
+      },
+      button: {
+        height: '50px',
+        width: '200px',
+        marginTop: '50px',
+        marginBottom: '15px'
+      }
+    };
+  }
+
   render() {
+    let styles = this.getStyles();
     return (
-      <div>
-        This is sign Up page.
+      <div style={styles.root}>
+        <form style={styles.form}>
+          <TextField
+            style={styles.textField}
+            floatingLabelText="用户名"
+            floatingLabelStyle={styles.floatingLabel} />
+          <TextField
+            style={styles.textField}
+            floatingLabelText="密码"
+            floatingLabelStyle={styles.floatingLabel}
+            type="password" />
+          <RaisedButton
+            style={styles.button}
+            labelStyle={styles.label}
+            type="submit"
+            label="注册"
+            secondary={true} />
+        </form>
       </div>
     );
   }
 }
 
-export default SignUp;
+export default Radium(SignUp);
+
--- imports/ui/styles/typography.js
+++ imports/ui/styles/typography.js
@@ -1,5 +1,6 @@
 export default typography = {
   fontFamily: 'sans-serif',
+  fontSmallSize: '20px',
   fontMiddleSize: '25px',
   fontWeightNormal: '300'
 };
