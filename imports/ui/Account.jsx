313235088658d3ea6b6364e4906f4501108955f6
Peter Wang <happypeter1983@gmail.com>
2016-05-21 17:32:48 +0800

    添加获取 GitHub 账号的表单

--- imports/ui/Account.jsx
+++ imports/ui/Account.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import Card from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import Radium from 'radium';
 
 class Account extends Component {
   render() {
+    let styles = {
+      root: {
+        flexGrow: 1,
+        overflowY: 'scroll',
+        paddingTop: '64px',
+        paddingLeft: '16px',
+        paddingRight: '16px'
+      },
+      card: {
+        maxWidth: '700px',
+        margin: '20px auto',
+        padding: '20px',
+      }
+    };
+
     return (
-      <div>
-        This is account page.
+      <div style={styles.root}>
+        <Card style={styles.card}>
+          <form>
+            <TextField
+              hintText="GitHub Account"
+              ref='username'/>
+
+            <FlatButton
+              type="submit"
+              primary={true}
+              label="search github" />
+          </form>
+        </Card>
       </div>
     );
   }
 }
 
-export default Account;
+export default Radium(Account);
