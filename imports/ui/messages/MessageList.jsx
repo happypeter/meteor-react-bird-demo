776d772f4588588a93927635da54c2d47670e261
Peter Wang <happypeter1983@gmail.com>
2016-05-23 22:02:33 +0800

    页面转圈加载效果

--- imports/ui/messages/MessageList.jsx
+++ imports/ui/messages/MessageList.jsx
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import map from 'lodash/fp/map';
 import Radium from 'radium';
+import CircularProgress from 'material-ui/CircularProgress';
 
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+  componentDidMount() {
+    $(".loader").delay(800).fadeOut('slow', function() {
+      $("#messages").fadeIn('slow');
+      let msgList = document.getElementById('message-list');
+      if(msgList !== null) {
+        msgList.scrollTop = msgList.scrollHeight;
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    let msgList = document.getElementById('message-list');
+    if(msgList !== null) {
+      msgList.scrollTop = msgList.scrollHeight;
+    }
+  }
+
   render() {
     let styles = {
+      circle: {
+        margin: '0 auto',
+        paddingTop: '100px',
+        display: 'block'
+      },
       list: {
         flexGrow: 1,
         overflowY: 'auto',
+      },
+      messages: {
+        display: 'none'
       }
     };
 
@@ -20,8 +46,12 @@ class MessageList extends Component {
     }, this.props.messages);
 
     return (
-      <div style={styles.list}>
-        <div>
+      <div id="message-list" style={styles.list}>
+        <CircularProgress
+          mode="indeterminate"
+          className="loader"
+          style={styles.circle} />
+        <div id="messages" style={styles.messages}>
           { allMessages }
         </div>
       </div>
